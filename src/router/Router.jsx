cc6bd4eacc287f94adf857b92b0c3137020faadf
hyeonw8 <whgusrud223@gmail.com>
2024-06-10 20:50:30 +0900

    ✨ update : Router 세팅 추가 및 수정
    auth path 추가 및 버전 v5 -> v6으로 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,20 +1,42 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
 import Layout from '../shared/Layout';
 import Home from "../pages/Home";
 import Detail from "../pages/Detail";
+import LoginForm from "../components/Auth/LoginForm";
+import SignUpForm from "../components/Auth/SignUpForm";
+import Auth from "../pages/Auth";
 
-const Router = () => {
 
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/detail/:id" element={<Detail />}/>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  )
-}
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'auth',
+        element: <Auth />,
+        children: [
+          {
+            path: 'login',
+            element: <LoginForm />
+          },
+          {
+            path: 'signup',
+            element: <SignUpForm />
+          }
+        ]
+      },
+      {
+        path: '/detail/:id',
+        element: <Detail />
+      },
+    ]
+  }
+]);
+
 
 export default Router
