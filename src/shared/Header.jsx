eb6d34e3b7c3126c3f1af68ebbbcda594a5213f2
hyeonw8 <whgusrud223@gmail.com>
2024-06-11 01:37:08 +0900

    ✨ update : 버튼 이름 한국어로 변경, 헤더 타이틀에 홈 이동 추가

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 const Header = () => {
   return (
     <StHeaderDiv>
-      <StHeaderTitle>✐ keep a record of household expenses </StHeaderTitle>
+      <StHeaderTitle><Link to='/'>✐ keep a record of household expenses</Link> </StHeaderTitle>
       <StBtnBox>
-        <StLogin> <Link to='/auth/login'>Login</Link></StLogin>
-        <StSignUpReact>
-          <Link to='/auth/signup'>SignUp</Link>
-        </StSignUpReact>
+        <StLogin> <Link to='/auth/login'>로그인</Link></StLogin>
+        <StSignUp>
+          <Link to='/auth/signup'>회원가입</Link>
+        </StSignUp>
       </StBtnBox>
     </StHeaderDiv>
   );
@@ -33,13 +33,20 @@ const StHeaderTitle = styled.h2`
   font-size: 20px;
   font-weight: bold;
   padding: 20px;
+
+  a {
+    color: black;
+  }
 `;
 
 const StBtnBox = styled.div`
   display: flex;
   flex-direction: row;
   column-gap: 10px;
+  margin-right: 15px;
 `
+
+// 한개로 합쳐보자
 const StLogin = styled.button`
   border-radius: 20px;
   width: 100px;
@@ -47,12 +54,14 @@ const StLogin = styled.button`
   background-color: black;
   color: white;
   font-size: 18px;
+  border: none;
 `;
-const StSignUpReact = styled.button`
+const StSignUp = styled.button`
   border-radius: 20px;
   width: 100px;
   height: 40px;
   background-color: black;
   color: white;
   font-size: 18px;
+  border: none;
 `
