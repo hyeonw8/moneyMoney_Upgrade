c218cbafe44970541ec382a4d5d59eb1e120c746
hyeonw8 <whgusrud223@gmail.com>
2024-06-11 22:03:37 +0900

    ✨ update : 로그인 여부에 따라 조건부로  헤더의 버튼 박스 부분을 보여주도록 기능 추가

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,19 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { logout } from '../redux/slices/authSlice';
 
 const Header = () => {
-  return (
-    <StHeaderDiv>
-      <StHeaderTitle><Link to='/'>✐ keep a record of household expenses</Link> </StHeaderTitle>
-      <StBtnBox>
-        <StLogin> <Link to='/auth/login'>로그인</Link></StLogin>
-        <StSignUp>
-          <Link to='/auth/signup'>회원가입</Link>
-        </StSignUp>
-      </StBtnBox>
-    </StHeaderDiv>
-  );
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  //console.log(isAuthenticated)
+  const dispatch = useDispatch();
+
+const handleLogout = () => {
+  localStorage.removeItem('accessToken');
+  dispatch(logout());
+  console.log(isAuthenticated)
 };
+
+return (
+  <StHeaderDiv>
+    <StHeaderTitle>
+      <Link to='/'>✐ keep a record of household expenses</Link>
+    </StHeaderTitle>
+    <StBtnBox>
+      {isAuthenticated ? (
+        <StLogin onClick={handleLogout} $text='로그아웃'>로그아웃</StLogin>
+      ) : (
+        <>
+          <StLogin $text='로그인'>
+            <Link to='/auth/login'>로그인</Link>
+          </StLogin>
+          <StSignUp>
+            <Link to='/auth/signup'>회원가입</Link>
+          </StSignUp>
+        </>
+      )}
+    </StBtnBox>
+  </StHeaderDiv>
+)
+}
 
 export default Header;
 
@@ -25,15 +47,15 @@ const StHeaderDiv = styled.div`
   height: 50px;
   padding: 10px;
   margin: 0 auto;
-  width: 100vw;
+  /* width: 100vw;
   max-width: 1200px;
-  min-width: 780px;
+  min-width: 780px; */
 `;
 const StHeaderTitle = styled.h2`
   font-size: 20px;
   font-weight: bold;
   padding: 20px;
-
+  cursor: pointer;
   a {
     color: black;
   }
@@ -43,7 +65,7 @@ const StBtnBox = styled.div`
   display: flex;
   flex-direction: row;
   column-gap: 10px;
-  margin-right: 15px;
+  margin-right: 20px;
 `
 
 // 한개로 합쳐보자
@@ -55,6 +77,7 @@ const StLogin = styled.button`
   color: white;
   font-size: 15px;
   border: none;
+  
 `;
 const StSignUp = styled.button`
   border-radius: 20px;
