35a437f4ab4ac0af420f64530a46cc88ed13c843
hyeonw8 <whgusrud223@gmail.com>
2024-06-11 21:59:59 +0900

    💄 style : 스타일 컴포넌트 속성 수정

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,24 +1,15 @@
-import styled from 'styled-components';
 import Header from '../shared/Header';
 import { Outlet } from 'react-router-dom';
 
-
 const Layout = () => {
   return (
-    <StLayout>
+    <>
       <Header />
       <Outlet />
-    </StLayout>
+    </>
   );
 };
 
 export default Layout;
 
-const StLayout = styled.div`
-  /* min-height: 100vh;
-  max-width: 1200px;
-  min-width: 780px;
-  height: 100vh;
-  width: 100vw;
-  margin: 0 auto; */
-`;
+
