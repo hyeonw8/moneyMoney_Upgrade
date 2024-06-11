c9aede2d0a9ba1e21e4c427178a5b3efd0e124ab
hyeonw8 <whgusrud223@gmail.com>
2024-06-11 22:04:49 +0900

    ✨ update : 회원가입, 로그인 API 연결 및 input 필드에 조건 추가

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,15 +1,47 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import api from '../../api/authAPI';
+import { login } from '../../redux/slices/authSlice';
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  //console.log(isAuthenticated)
+  const dispatch = useDispatch();
 
+  const [ id, setId ] = useState('');
+  const [ password, setPassword ] = useState('');
 
-  const handleSubmitForm = (e) => {
+  const handleSubmitForm = async (e) => {
     e.preventDefault();
-    
-    navigate('/')
-  }
+
+    try {
+      const response = await api.post('/login',
+        {
+          id,
+          password,
+        }
+      );
+      const data = response.data;
+      console.log(data);
+      if(data.success) {
+        alert('로그인 되었습니다!');
+        localStorage.setItem('accessToken', data.accessToken);
+        dispatch(login());
+
+        navigate('/');
+      } else {
+        alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+      }
+    } catch(error) {
+      console.error("Signup error:", error);
+      alert('로그인에 오류가 발생했습니다. 다시 시도해 주세요');
+      // 토스트나 스윗으로 바꿔보기
+    }
+  };
+  
 
   return (
     <>
@@ -18,15 +50,35 @@ const LoginForm = () => {
           <StTitle>로그인</StTitle>
           <StInputBox>
             {/* <label htmlFor="inputID">아이디</label> */}
-            <StInput type="text" id="inputID" placeholder='아이디' minLength={4} maxLength={10}/>
+            <StInput 
+              type="text" 
+              id="inputID" 
+              placeholder="아이디" 
+              minLength={4} 
+              maxLength={10}
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+            />
           </StInputBox>
           <StInputBox>
             {/* <label htmlFor="inputPWD">비밀번호</label> */}
-            <StInput type="password" id="inputPWD" placeholder='비밀번호'  minLength={4} maxLength={15}/>
+            <StInput 
+              type="password" 
+              id="inputPWD" 
+              placeholder="비밀번호" 
+              minLength={4} 
+              maxLength={15}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </StInputBox>
-          <StButton type='submit' $text='login'>로그인하기</StButton>
-          <StButton type='button' $text='signUp'>
-            <Link to='/auth/signup'>회원가입하러 가기</Link>
+          <StButton type="submit" $text="login">
+            로그인하기
+          </StButton>
+          <StButton type="button" $text="signUp">
+            <Link to="/auth/signup">회원가입하러 가기</Link>
           </StButton>
         </StLoginForm>
       </StLoginWrapper>
@@ -40,7 +92,9 @@ const StLoginWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  margin: 0 auto;
+  padding: 10px;
+  position: relative;
 `;
 
 const StLoginForm = styled.form`
@@ -53,7 +107,12 @@ const StLoginForm = styled.form`
   height: 400px;
   background-color: #ffffffdd;
   border-radius: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  top: 50%;
+  transform: translate(0%, 50%);
 `;
+
 const StInputBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -77,7 +136,7 @@ const StInput = styled.input`
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
   text-align: left;
   padding-left: 10px;
-`
+`;
 const StButton = styled.button`
   width: 410px;
   height: 50px;
@@ -88,4 +147,5 @@ const StButton = styled.button`
   font-size: 18px;
   margin-top: 15px;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
 `;
--- src/components/Auth/SignUpForm.jsx
+++ src/components/Auth/SignUpForm.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import api from '../../api/authAPI';
 
 const SignUpForm = () => {
   const navigate = useNavigate();
 
+  const [ id, setId ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ nickname, setNickname ] = useState('');
 
-  const handleSubmitForm = (e) => {
+  const handleSubmitForm = async (e) => {
     e.preventDefault();
     
-    navigate('/');
+    try {
+      const response = await api.post('/register',
+        {
+          id,
+          password,
+          nickname,
+        }
+      );
+      const data = response.data;
+     
+      if(data.success) {
+        alert('회원가입이 완료되었습니다. 환영합니다!');
+        navigate('/');
+      } else {
+        alert('회원가입에 실패했습니다. 다시 시도해 주세요.');
+      }
+    } catch(error) {
+      console.error("Signup error:", error);
+      alert('회원가입에 오류가 발생했습니다. 다시 시도해 주세요');
+      // 토스트나 스윗으로 바꿔보기
+    }
+
+    
   }
 
   return (
@@ -18,15 +45,42 @@ const SignUpForm = () => {
           <StTitle>회원가입</StTitle>
           <StInputBox>
             {/* <StLabel htmlFor="inputID">아이디</StLabel> */}
-            <StInput type="text" id="inputID" placeholder='아이디'  minLength={4} maxLength={10}/>
+            <StInput 
+              type="text" 
+              id="inputID" 
+              placeholder='아이디'
+              minLength={4}
+              maxLength={10}
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+            />
           </StInputBox>
           <StInputBox>
             {/* <StLabel htmlFor="inputPWD">비밀번호</StLabel> */}
-            <StInput type="password" id="inputPWD" placeholder='비밀번호' minLength={4} maxLength={15}/>
+            <StInput 
+              type="password" 
+              id="inputPWD" 
+              placeholder='비밀번호' 
+              minLength={4} 
+              maxLength={15}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </StInputBox>
           <StInputBox>
             {/* <StLabel htmlFor="inputName">닉네임</StLabel> */}
-            <StInput type="text" id="inputName" placeholder='닉네임' minLength={1} maxLength={10}/>
+            <StInput 
+              type="text" 
+              id="inputName" 
+              placeholder='닉네임' 
+              minLength={1} 
+              maxLength={10}
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              required
+            />
           </StInputBox>
           <StButton type='submit' $text='signup'>회원가입하기</StButton>
           <StButton type='button' $text='login'>
@@ -44,7 +98,9 @@ const StSignUpWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  margin: 0 auto;
+  padding: 10px;
+  position: relative;
 `;
 
 const StSignUpForm = styled.form`
@@ -57,6 +113,10 @@ const StSignUpForm = styled.form`
   height: 450px;
   background-color: #ffffffdd;
   border-radius: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  top: 50%;
+  transform: translate(0%, 40%);
 `;
 
 const StInputBox = styled.div`
@@ -94,4 +154,5 @@ const StButton = styled.button`
   font-size: 18px;
   margin-top: 15px;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
 `;
