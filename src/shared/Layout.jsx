4fc94de3bd3796b3fde095e5c26b8a632c4671a5
hyeonw8 <whgusrud223@gmail.com>
2024-06-13 02:56:52 +0900

    ✨ update : 처음 렌더링 시  유저 상태 확인 및 데이터 가져오기

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,11 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Header from '../shared/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate  } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getUserInfoAPI } from '../api/auth';
+import { setUserData } from '../redux/slices/authSlice';
+import { toast } from 'react-toastify';
 
 const Layout = () => {
+  const userData = useSelector((state) => state.auth.userData);
+  console.log('유저 정보 확인(layout에서 로그인 상태 확인 중) ', userData);
+  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await getUserInfoAPI();
+        const data = response.data;
+
+        dispatch(setUserData({ userId: data.id, nickname: data.nickname, avatar: data.avatar }))
+        console.log('현재 로그인된 유저가 있나요?', data);
+        console.log(userData);
+
+    
+      } catch (error) {
+        console.error('유저 정보 가져오기 오류:', error);
+        toast.error(error?.response?.data?.message);
+        dispatch(setUserData(null));
+       //navigate('/login');
+        localStorage.removeItem('accessToken');
+        navigate('/login');
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet /> 
     </>
   );
 };
