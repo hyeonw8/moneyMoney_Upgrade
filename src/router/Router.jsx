3721e06ed05c1ea548445891c20c2a0be527aba3
hyeonw8 <whgusrud223@gmail.com>
2024-06-13 02:40:48 +0900

    ✨ update : 라우터 설정 변경, PrivateRoute 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,42 +1,81 @@
-import { createBrowserRouter } from "react-router-dom"
+//import { createBrowserRouter } from "react-router-dom"
 import Layout from '../shared/Layout';
-import Home from "../pages/Home";
-import Detail from "../pages/Detail";
-import LoginForm from "../components/Auth/LoginForm";
-import SignUpForm from "../components/Auth/SignUpForm";
-import Auth from "../pages/Auth";
+import Home from '../pages/Home';
+import Detail from '../pages/Detail';
+import LoginForm from '../components/Auth/LoginForm';
+import SignUpForm from '../components/Auth/SignUpForm';
+import MyPage from '../pages/MyPage';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+//import Auth from '../pages/Auth';
 
+// const Router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />,
+//       },
+//       {
+//         path: 'auth',
+//         element: <Auth />,
+//         children: [
+//           {
+//             path: 'login',
+//             element: <LoginForm />
+//           },
+//           {
+//             path: 'signup',
+//             element: <SignUpForm />
+//           }
+//         ]
+//       },
+//       {
+//         path: '/detail/:id',
+//         element: <Detail />
+//       },
+//       {
+//         path: 'mypage',
+//         element: <PrivateRoute />,
+//         children: [
+//           {
+//             path: '',
+//             element: <MyPage />,
+//           },
+//         ],
+//       },
+//     ]
+//   }
+// ]);
 
-const Router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: 'auth',
-        element: <Auth />,
-        children: [
-          {
-            path: 'login',
-            element: <LoginForm />
-          },
-          {
-            path: 'signup',
-            element: <SignUpForm />
-          }
-        ]
-      },
-      {
-        path: '/detail/:id',
-        element: <Detail />
-      },
-    ]
-  }
-]);
+// const PublicRoute = ({element}) => {
+//   const isLogin = useSelector((state) => state.auth.accessToken);
+  
+//   return isLogin ? <Navigate to='/' /> : element;
+// }
 
+const PrivateRoute = ({element}) => {
+  const isLogin = useSelector((state) => state.auth.accessToken);
+  
+  return isLogin ?  element : <Navigate to='/login' />;
+}
 
-export default Router
+const Router = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PrivateRoute element={<Home />} />} />
+          <Route path="detail/:id" element={<PrivateRoute element={<Detail />} />} />
+          <Route path="mypage"  element={<PrivateRoute element={<MyPage />} />} />
+          <Route path="signup" element={<SignUpForm />} />
+          <Route path="login" element={<LoginForm />} />
+        </Route>
+        </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
