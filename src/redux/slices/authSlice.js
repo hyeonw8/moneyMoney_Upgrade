641429b13e4627116f1d279b05ba89449d351f4a
hyeonw8 <whgusrud223@gmail.com>
2024-06-13 02:53:09 +0900

    ✨ update : 초기값 추가, 리듀서 추가 및 수정

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -2,20 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isAuthenticated: false,
+  accessToken: localStorage.getItem("accessToken") || null,
+  userData : null,
 }
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state) => {
+    login: (state, action) => {
       state.isAuthenticated = true;
+      state.accessToken = action.payload;
     },
     logout: (state) => {
       state.isAuthenticated = false;
+    },
+    setUserData: (state, action) => {
+      state.userData = action.payload;
     }
   }
 })
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, setUserData } = authSlice.actions;
 export default authSlice.reducer;
