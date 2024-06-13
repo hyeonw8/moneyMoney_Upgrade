449be0c0395635523778dcf80c694dd53266be1a
hyeonw8 <whgusrud223@gmail.com>
2024-06-14 02:03:49 +0900

    🩹 fix : Tanstack Query 적용 - useQuery

--- src/components/List/MoneyList.jsx
+++ src/components/List/MoneyList.jsx
@@ -1,6 +1,59 @@
 import styled from 'styled-components';
 import MoneyItem from './MoneyItem';
 import { useSelector } from 'react-redux';
+import { useQuery } from '@tanstack/react-query';
+import { getItemsAPI } from '../../api/dataAPI';
+
+const MoneyList = () => {
+  //const datas = useSelector((state) => state.datas.data);
+  const selectedMonth = useSelector((state) => state.datas.selectedMonth);
+
+  const { data: expenses, isLoading, isError } = useQuery({
+  queryKey: ['expenses'],
+  queryFn: getItemsAPI,
+  });
+  // console.log('isLoading >>> ',isLoading );
+  // console.log('expenses >>> ',expenses);
+  // console.log('isError >>> ',isError);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error occurred while fetching data.</div>;
+  }
+
+  const filteredData = expenses?.filter((data) => {
+    return data.month === selectedMonth;
+  });
+
+  if (filteredData.length === 0) {
+    return (
+      <StNotMoneyList>
+        <h2> 해당 월에는 지출 내역이 없습니다! </h2>
+        <p> 지출 내역이 있다면 추가해주세요 📝 </p>
+      </StNotMoneyList>
+    );
+  }
+
+  const sortedData = filteredData.sort((data1, data2) => {
+    if (new Date(data2.date) - new Date(data1.date) === 0) {
+      return Number(data2.cost) - Number(data1.cost) 
+    }
+    return new Date(data2.date) - new Date(data1.date);
+  });
+
+  return (
+    <StMoneyList>
+      {sortedData.map((data) => (
+        <MoneyItem key={data.id} data={data} />
+      ))}
+    </StMoneyList>
+  );
+};
+
+export default MoneyList;
 
 const StMoneyList = styled.div`
   height: 100%;
@@ -23,39 +76,3 @@ const StNotMoneyList = styled.div`
   line-height: 30px;
   font-weight: 500;
 `;
-
-const MoneyList = () => {
-  const datas = useSelector((state) => state.datas.data);
-  const selectedMonth = useSelector((state) => state.datas.selectedMonth);
-
-  const filteredData = datas.filter((data) => {
-    const spliceMonth = data.date.slice(5, 7);
-    return Number(spliceMonth) === selectedMonth;
-  });
-
-  const sortedData = filteredData.sort((data1, data2) => {
-    if (new Date(data2.date) - new Date(data1.date) === 0) {
-      return Number(data2.cost) - Number(data1.cost) 
-    }
-    return new Date(data2.date) - new Date(data1.date);
-  });
-
-  if (filteredData.length === 0) {
-    return (
-      <StNotMoneyList>
-        <h2> 해당 월에는 지출 내역이 없습니다! </h2>
-        <p> 지출 내역이 있다면 추가해주세요 📝 </p>
-      </StNotMoneyList>
-    );
-  }
-
-  return (
-    <StMoneyList>
-      {sortedData.map((data) => (
-        <MoneyItem key={data.id} data={data} />
-      ))}
-    </StMoneyList>
-  );
-};
-
-export default MoneyList;
