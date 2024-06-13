f916c9480a1c04bd7dc33ecc9369795a78ea8a7a
hyeonw8 <whgusrud223@gmail.com>
2024-06-14 01:25:35 +0900

    ✨ update : Tanstack Query, react-toastify 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/config/configStore';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import QueryClientSetup from './QueryClientSetup';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <QueryClientSetup>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer autoClose={1800} limit={1} closeOnClick />
+      <App />  
+    </QueryClientSetup>
+  </Provider>
+  // </React.StrictMode>,
 )
