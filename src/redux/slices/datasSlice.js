78c5c8f8725ec5b41371a3c77fba358a6b676595
hyeonw8 <whgusrud223@gmail.com>
2024-06-14 11:34:25 +0900

    🩹 fix : json-server 이용으로 인해 로컬 데이터 파일 삭제 및 상태관리 제거

--- src/redux/slices/datasSlice.js
+++ src/redux/slices/datasSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit"
-import moneyData from '/src/moneyData.json';
 
 const getSelectedMonth = () => {
   return JSON.parse(localStorage.getItem('month')) || []
 }
 
 const initialState = {
-  data: moneyData,
   selectedMonth: getSelectedMonth(),
 }
 
@@ -17,27 +15,8 @@ const datasSlice = createSlice({
     setSelectedMonth: (state, action) => {
       state.selectedMonth = action.payload;
     },
-    addData: (state, action) => {
-      state.data.push(action.payload);
-    },
-    deleteData: (state, action) => {
-      const id = action.payload;
-      const idx = state.data.findIndex(item => item.id === id);
-
-      if (idx !== -1) {
-        state.data.splice(idx, 1); 
-      }
-    },
-    updateData: (state, action) => {
-      const { id, updatedItem } = action.payload;
-      const idx = state.data.findIndex(item => item.id === id);
-
-      if( idx !== -1 ) {
-        state.data[idx] = updatedItem; 
-      }
-    }
   }
 })
 
-export const { setSelectedMonth, addData, deleteData, updateData } = datasSlice.actions;
+export const { setSelectedMonth } = datasSlice.actions;
 export default datasSlice.reducer;
